b4c5825f86da9a0d745f5204c701bca9bdcb8ac2
Ana Beatriz Oliveira <ana.bo22@aluno.ifsc.edu.br>
2022-09-18 15:57:09 -0300

    atualizações.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDMsL6GXPwyTl6tAtDbgQfAuEDfAv724I8",
+    authDomain: "requisicoes-app-70794.firebaseapp.com",
+    projectId: "requisicoes-app-70794",
+    storageBucket: "requisicoes-app-70794.appspot.com",
+    messagingSenderId: "195937318423",
+    appId: "1:195937318423:web:125b501c442bcaac980e52"
+  }
 };
